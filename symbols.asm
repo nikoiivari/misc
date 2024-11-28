86a5afcdd7bdb47cc57d14fd0b454260234f0679
Niko Iivari <nxu@huoleton.net>
2024-11-28 15:54:39 +0200

    Reinterpret scope

--- symbols.asm
+++ symbols.asm
@@ -19,14 +19,16 @@ scope main
     var bar:    pack      .wxyz   4byte   unsigned    # .wxyz gets packed into
                                                       # the previous dword.
                                                       # It will be even.
-    in argc:    odd   .stuvwxyz   8byte   signed
-    in argv[]:  even  .stuvwxyz   8byte   capability  # argv[] should use string?
+    
+    var sthing: odd   .stuvwxyz   8byte   capability  # to store instance of scope something
 
-    out status: odd          .z   1byte   signed
-epocs
+    in argc:    even   .stuvwxyz   8byte   signed
+    in argv[]:  odd    .stuvwxyz   8byte   capability # argv[] should use string?
+
+    out status: even          .z   1byte   signed
 
 # main is a label, but the fun keyword makes it a function label.
-fun main (argc, argv):
+fun main: (argc, argv)
 
     accum = @           # save accumulator
     @ = @[0:0...3]      # Square brackets '[' and ']' imply indexing -- not direct memory access
@@ -40,7 +42,26 @@ fun main (argc, argv):
                         # will be replicated on all the current region accumulator registers
                         # in groups of four registers.
     
-    status = 0          # Gotta always return a status byte from main. Something has to be written
-                        # to status before the function exits.
+    status = 00h        # Gotta always return a status byte from main. Default to 00h.
+                        # Something has to be written to status before the function exits.
     
-nuf (status)            # fun main ends, returning status.
+    sthing = instance something     # var sthing is in scope main. Scope main is in the heap.
+    status = [sthing do_sth: 00h]   # call method do_sth: of instance sthing
+
+nuf status              # fun main ends, returning status.
+epocs                   # scope main ends
+
+scope something
+    in p1:  even  .stuvwxyz     8byte   unsigned
+    out r1: odd          .z     1byte   signed
+
+    fun do_sth: p1
+        r1 = p1         # Most useless, but whatever...
+    nuf r1
+
+    out r2: even         .z     1byte   signed
+
+    fun do_sth_else
+        r2 = 01h        # Return something
+    nuf r2
+epocs
