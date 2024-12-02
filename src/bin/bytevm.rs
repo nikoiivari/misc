4d29f61003f00ad3f3372e2f84fe540ff8ea0b2c
Niko Iivari <nxu@huoleton.net>
2024-12-02 14:45:06 +0200

    Add bytevm.rs

---
+++ src/bin/bytevm.rs
@@ -0,0 +1,27 @@
+#![allow(dead_code, unused_imports)]
+
+use std::env;
+use std::fs;
+
+struct Reg {
+    wbr: bool,
+    cap: bool,
+    bits:u64,
+}
+
+// Bytecode virtual machine
+// Read bytecode file and execute.
+
+fn main () {
+    // Cache of 4096 registers. (16 pages of 256 each). ~32KB cache
+    let mut _cache: [Reg; 4096];
+
+    // Accum of 4096 registers. (16 pages of 256 each). ~32KB accum
+    let mut _accum: [Reg; 4096];
+
+    
+    // File loading and setup that an OS would do.
+    // TODO: get filename from commandline!!!
+    let _buffer: Vec<u8> = fs::read("symbols.asm").unwrap();
+
+}
