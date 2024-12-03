afee951cf59703d340d1e03c8abe95d1458f19dc
Niko Iivari <nxu@huoleton.net>
2024-12-03 15:52:57 +0200

    codesize is broken. Fix codesize.

--- src/bin/bytevm.rs
+++ src/bin/bytevm.rs
@@ -22,6 +22,15 @@ fn main () {
     
     // File loading and setup that an OS would do.
     // TODO: get filename from commandline!!!
-    let _buffer: Vec<u8> = fs::read("symbols.exe").unwrap();
-
+    let buffer: Vec<u8> = fs::read("symbols.exe").unwrap();
+    let mut codesize:u32 = buffer[7] as u32; //lowest byte
+    let cs2b: u32 = buffer[6] as u32;
+    let cs3b: u32 = buffer[5] as u32;
+    println!("{:?}", codesize);
+    codesize = codesize & (cs2b << 8);
+    println!("{:?}", codesize);
+    codesize = codesize & (cs3b << 16); // 24bits of codesize
+    println!("{:?}", codesize);
+    // Put code into cache registers
+    //TODO: codesize is broken. fix codesize.
 }
